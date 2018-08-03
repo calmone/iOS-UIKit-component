d27671d7a81d8740ba5c5a5d9a9169cae19a970b
calmone <calmone91@gmail.com>
2018-08-03 10:13:55 +0900

    Add UIPickerView

--- UIKit component handling/ComponentViewController/PickerViewViewController.swift
+++ UIKit component handling/ComponentViewController/PickerViewViewController.swift
@@ -8,12 +8,36 @@
 
 import UIKit
 
-class PickerViewViewController: UIViewController {
+class PickerViewViewController: BaseViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    lazy var pickerView: UIPickerView = {
+        // Generate UIPickerView.
+        let picker = UIPickerView()
+        
+        // Specify the size.
+        picker.frame = CGRect(x: 0, y: 150, width: self.view.bounds.width, height: 180.0)
+        
+        // Set the backgroundColor.
+        picker.backgroundColor = .white
+        
+        // Set the delegate.
+        picker.delegate = self
+        
+        // Set the dataSource.
+        picker.dataSource = self
+        
+        return picker
+    }()
+    
+    private let values: [String] = ["A","B","C","D","E","F","G","H","I"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // Add UIPickerView on view
+        self.view.addSubview(self.pickerView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +45,25 @@ class PickerViewViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
+    
+    // Data source method that returns the number of rows to display in the picker.
+    // (Implementation required)
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return values.count
+    }
+    
+    // Delegate method that returns the value to be displayed in the picker.
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return values[row]
+    }
+    
+    // A method called when the picker is selected.
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("row: \(row)")
+        print("value: \(values[row])")
+    }
 
 }
