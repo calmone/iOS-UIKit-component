5fba3bdcdb01a2d149d0b87a22c688c05fcc7b9c
calmone <calmone91@gmail.com>
2018-08-20 13:20:16 +0900

    Add Resize UIImage

--- UIKit component handling/ComponentViewController/ResizeImageVC.swift
+++ UIKit component handling/ComponentViewController/ResizeImageVC.swift
@@ -8,28 +8,52 @@
 
 import UIKit
 
-class ResizeImageVC: UIViewController {
+class ResizeImageVC: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // Prepare the UIImage to be resized.
+        let image = UIImage(named: "vertical.jpeg")!
+        
+        // Prepare UIImage after resizing.
+        let resize = image.ResizeUIImage(width: self.view.frame.midX, height: self.view.frame.midY)
+        
+        // Set the UIImage after resizing to UIImageView.
+        let imageView = UIImageView(image: resize)
+        
+        imageView.layer.position = CGPoint(x: self.view.frame.midX, y: self.view.frame.midY)
+        
+        self.view.addSubview(imageView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+}
+
+fileprivate extension UIImage {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Create a class method of Resize.
+    func ResizeUIImage(width: CGFloat, height: CGFloat) -> UIImage! {
+        
+        // Prepare the context of the specified image size.
+        UIGraphicsBeginImageContext(CGSize(width: width, height: height))
+        
+        // Draw the image in the context.
+        self.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+        
+        // Create a UIImage from the context.
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        // Close the context.
+        UIGraphicsEndImageContext()
+        
+        return newImage
     }
-    */
-
+    
 }
