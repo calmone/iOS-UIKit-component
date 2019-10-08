9cceb502876d3cc16faebb4661956e101930bc2d
한태현 <thhan@pentasecurity.com>
2019-10-08 15:56:08 +0900

    Fix hasValue -> rawValue

--- UIKit component handling/RootViewController.swift
+++ UIKit component handling/RootViewController.swift
@@ -29,7 +29,7 @@ class RootViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return ComponentList.COUNT.hashValue
+        return ComponentList.COUNT.rawValue
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
